0744a232b48778775b9408b72d22747e8c7172ea
Agata Siemieniec <agata.siemieniec@hotmail.com>
2020-10-30 01:46:30 +0100

    Add render method and accordion method to hide options of  product

--- src/js/script.js
+++ src/js/script.js
@@ -58,9 +58,62 @@
 
       thisProduct.id = id;
       thisProduct.data = data;
-      
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
       console.log('new Product:', thisProduct);
     }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      /*  generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      
+      /*  create element using utils.createElementFromHTMl */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      
+      /*  find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      
+      /*  add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function(event){
+        console.log('clicked');
+      
+        /* prevent default action for event */
+        event.preventDefault();
+        /* ? toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        console.log(thisProduct.element.classList.contains(classNames.menuProduct.wrapperActive));
+        /* find all active products */
+        const activeProducts = document.querySelectorAll('.product.active');
+        /* START LOOP: for each active product */
+        for(let activeProduct of activeProducts){
+          /* START: if the active product isn't the element of thisProduct */
+          if(activeProducts !== thisProduct.element){
+            /* remove class active for the active product */
+            activeProduct.remove('.product.active');
+            console.log(activeProduct.contains('.product.active'));
+          /* END: if the active product isn't the element of thisProduct */
+          } 
+        /* END LOOP: for each active product */
+        }
+      /* END: click event listener to trigger */
+      });
+
+    }
+
   }
 
   const app = {
