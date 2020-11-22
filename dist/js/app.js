3cf429b6022b18890a1527f044bf92ae0e7b0106
Agata Siemieniec <agata.siemieniec@hotmail.com>
2020-11-22 08:00:50 +0100

    Add Booking class

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
 
@@ -103,6 +104,14 @@ const app = {
     });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    const bookingWrapper = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWrapper);
+
+  },
+
   init: function(){
     const thisApp = this;
 
@@ -115,6 +124,7 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
 
   },
 };
